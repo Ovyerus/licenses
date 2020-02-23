d5b2304e8aedc1c7eeb6305ca5745aad0a5cd36f
Ovyerus <iamovyerus@gmail.com>
2020-02-23 15:40:12 +1100

    chore: cleanup make.js a bit

--- make.js
+++ make.js
@@ -1,15 +1,13 @@
-"use strict";
-require("hard-rejection/register"); // eslint-disable-line import/no-unassigned-import
-const Ora = require("ora");
-const got = require("got");
-const pMap = require("p-map");
 const { yellow } = require("colorette");
+const got = require("got");
+const Ora = require("ora");
 
-const fs = require("fs");
+const { promises: fs } = require("fs");
 const path = require("path");
 
 const replacers = require("./replacers");
 
+// TODO: put this in proper files
 const typesTemplate = `declare namespace licenses {
   export type Identifiers = {{}};
 
@@ -38,115 +36,141 @@ declare const license: License;
 export default license;
 `;
 
-const URL = "https://spdx.org/licenses/licenses.json";
-const MAX_CONCURRENT_CONNECTIONS = 10;
-const spinner = new Ora();
+const replaceText = (replacer, text) => {
+  let newText = text;
 
-spinner.start();
+  for (const [name, regex_] of Object.entries(replacer)) {
+    const regex = Array.isArray(regex_) ? regex_ : [regex_];
+    let replacement;
 
-(async () => {
-  const response = await got(URL, { json: true });
+    // removeX handling - just blank it out
+    if (name.startsWith("remove")) replacement = "";
+    else if (name === "replaceCopyrightInfo")
+      // replaceCopyrightInfo special casing
+      replacement = "Copyright (c) <year> <author>";
+    else replacement = (match, group) => match.replace(group, `<${name}>`);
 
-  const spdx = response.body;
-  const licensesJson = Object.create(null);
+    // Go through the license text and run the replacement across all matching instances
+    for (const re of regex) newText = text.replace(re, replacement);
+  }
 
-  for (const license of spdx.licenses)
-    licensesJson[license.licenseId] = {
-      name: license.name,
-      url: Array.isArray(license.seeAlso)
-        ? license.seeAlso[0] // Only get the first of possibly multiple URLs
-        : `https://spdx.org/licenses/${license.licenseId}.html`, // Handle missing seeAlso
-      osiApproved: license.isOsiApproved
-    };
+  return newText;
+};
 
-  fs.writeFileSync("spdx.json", JSON.stringify(licensesJson, null, "\t"));
-  fs.writeFileSync(
-    "spdx-simple.json",
-    JSON.stringify(Object.keys(licensesJson), null, "\t")
+const chunk = (arr, n) =>
+  Array.from({ length: Math.ceil(arr.length / n) }, (v, i) =>
+    // eslint-disable-next-line no-mixed-operators
+    arr.slice(i * n, i * n + n)
   );
 
+const URL = "https://spdx.org/licenses/licenses.json";
+const spinner = new Ora().start();
+
+(async () => {
+  spinner.text = "Clearing `./licenses/`";
+
+  await fs.rmdir("./licenses", { recursive: true });
+  await fs.mkdir("./licenses");
+
+  spinner.text = "Fetching license list";
+
+  const { body } = await got(URL, { responseType: "json" });
+  const licenses = {};
+
+  for (const {
+    licenseId: id,
+    name,
+    seeAlso,
+    isOsiApproved: osiApproved
+  } of body.licenses)
+    licenses[id] = {
+      name,
+      osiApproved,
+      url: Array.isArray(seeAlso)
+        ? seeAlso[0] // Only pick first of possibly multiple URLS
+        : `https://spdx.org/licenses/${id}.html` // Handle missing seeAlso
+    };
+
+  await Promise.all([
+    fs.writeFile("spdx.json", JSON.stringify(licenses, null, "\t")),
+    fs.writeFile(
+      "spdx-simple.json",
+      JSON.stringify(Object.keys(licenses), null, "\t")
+    )
+  ]);
+
   let counter = 0;
+  const licenseLen = body.licenses.length;
+  const allLicenseData = [];
 
-  const mapper = async license => {
-    try {
-      const response = await got(license.detailsUrl, { json: true });
-      const currLicense = licensesJson[license.licenseId];
+  // Download the data for each license.
+  // Puts into batches as its faster than raw serial, however too many at
+  // once gets you banned from spdx.org for a while.
+  /* eslint-disable no-loop-func, no-await-in-loop */
+  for (const chnk of chunk(body.licenses, 20))
+    await Promise.all(
+      chnk.map(async l => {
+        const { body } = await got(l.detailsUrl, { responseType: "json" });
+        spinner.text = `Downloaded ${++counter}/${licenseLen}`;
 
-      currLicense.licenseText = response.body.licenseText
+        allLicenseData.push(body);
+      })
+    );
+  /* eslint-enable */
+
+  counter = 0;
+
+  // Manipulate all the data at once, running replacers & writing files.
+  await Promise.all(
+    allLicenseData.map(async body => {
+      const replacer = replacers[body.licenseId];
+      let licenseText = body.licenseText // Do some cleanup of the body text
         .replace(/\r\n/g, "\n")
         .replace(/http\ss\s:\/\//g, "https://")
         .trim();
 
-      const replacer = replacers[license.licenseId];
-
-      if (replacer)
-        for (const [name, regex_] of Object.entries(replacer)) {
-          let replacement, regex;
-
-          if (!Array.isArray(regex_)) regex = [regex_];
-          else regex = regex_;
-
-          if (name.startsWith("remove")) replacement = "";
-          else if (name === "replaceCopyrightInfo")
-            replacement = "Copyright (c) <year> <author>";
-          else
-            replacement = (match, group) => match.replace(group, `<${name}>`);
-
-          for (const re of regex)
-            currLicense.licenseText = currLicense.licenseText.replace(
-              re,
-              replacement
-            );
-        }
+      if (replacer) licenseText = replaceText(replacer, licenseText);
       else
         console.warn(
           yellow(
-            `\nNo placeholder replacer found for \`${license.licenseId}\`. Probably new license.`
+            `\nNo placeholder replacer found for \`${body.licenseId}\`. Probably new license.`
           )
         );
 
-      fs.writeFileSync(
-        path.join("licenses", `${license.licenseId}.json`),
-        JSON.stringify(currLicense, null, "\t")
+      licenses[body.licenseId].licenseText = licenseText;
+      const fp = path.join("licenses", `${body.licenseId}.json`);
+
+      await fs.writeFile(
+        fp,
+        JSON.stringify(licenses[body.licenseId], null, "\t")
       );
+      await fs.writeFile(`${fp}.d.ts`, licenseTypeTemplate);
 
-      fs.writeFileSync(
-        path.join("licenses", `${license.licenseId}.json.d.ts`),
-        licenseTypeTemplate
-      );
+      spinner.text = `${++counter}/${licenseLen}`;
+    })
+  );
 
-      spinner.text = `Downloaded ${++counter} of ${
-        Object.keys(licensesJson).length
-      } licenses`;
-    } catch (error) {
-      if (error.response)
-        throw new Error(
-          `Error getting URL ${license.detailsUrl}. Response is:\n${error.response.body}`
-        );
-      else throw error;
-    }
-  };
+  spinner.text = "Writing final files";
 
-  await pMap(spdx.licenses, mapper, {
-    concurrency: MAX_CONCURRENT_CONNECTIONS
-  });
-
-  fs.writeFileSync("spdx-full.json", JSON.stringify(licensesJson, null, "\t"));
-  fs.writeFileSync(
+  await fs.writeFile("spdx-full.json", JSON.stringify(licenses, null, "\t"));
+  await fs.writeFile(
     "index.d.ts",
     typesTemplate.replace(
       "{{}}",
-      JSON.stringify(Object.keys(licensesJson), null, "    ")
+      JSON.stringify(Object.keys(licenses), null, "    ")
     )
   );
 
-  spinner.text = "Writing type definitions";
-
   spinner.succeed("Done");
-})().catch(error => {
-  if ("response" in error)
-    throw new Error(
-      `Error getting URL ${error.url}. Response is:\n${error.response.body}`
-    );
-  else throw error;
-});
+})()
+  .catch(err => {
+    if (err.response)
+      throw new Error(
+        `Error getting URL ${err.url}. Response is:\n${err.response.body}`
+      );
+    else throw err;
+  })
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
